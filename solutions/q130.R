cb805f1875e84e0c5e0e0ac4ece0e2e98937b3b0
Enoch Yi-Tung Chen <enochytchen@gmail.com>
2021-04-24 17:05:21 +0200

    q130-a updated

--- solutions/q130.R
+++ solutions/q130.R
@@ -49,12 +49,12 @@ melanoma <- read_dta("melanoma.dta")%>%
 
 # Restrict follow-up to 10 years and create new variable for all-cause death and cause-specific death
 melanoma_10y <- mutate(melanoma,
-                       status  = ifelse(status == 1 | status == 2, 1, 0),# Redefine status
-                       surv_mm = ifelse(surv_mm<=120, surv_mm, 120),     # censored everyone after 120 months
-                       t = surv_mm/12                                    # surv_mm in years
+                       status  = ifelse((status == 1 | status == 2) & surv_mm<= 120, 1, 0), # Redefine status
+                       surv_mm = ifelse(surv_mm<= 120, surv_mm, 120),     # censored everyone after 120 months
+                       t = surv_mm/12                                     # surv_mm in years
                        )
 
-## Split the data with narrow (1 month) time intervals
+##(a)-i. Split the data with narrow (1 month) time intervals
 # Split the time scale
 melanoma_10y_spl <- survSplit(melanoma_10y, cut=1/12*(0:120), end="t", start="start", event = "status" ) # event must be a character variable
 
@@ -64,17 +64,30 @@ melanoma_10y_spl <- mutate(melanoma_10y_spl,
                            fu = as.factor(start) )
 ## Collapse
 melanoma_10y_spl2 <- melanoma_10y_spl %>%
-                     group_by(fu, female, year8594, agegrp) %>%
-                     summarise(pt=sum(pt), d = sum(status == 1)) %>%
-                     data.frame %>%
-                     group_by(fu) %>%
-                     mutate(interval = group_indices()) ## equivalent to Stata's egen interval=group()
+                     group_by_all(fu, female, year8594, agegrp) %>%
+                     summarise(pt=sum(pt), d = sum(status == 1)) 
 
-## TODO: Why there were more people died at the last interval??
-## The model looks right but output is different from Stata.
-##(a) Fit a model with a parameter for each interval
-poisson_a <- glm( d ~ as.factor(interval) + offset( log(pt) ),
+## Generate interval 
+melanoma_10y_spl2 <- melanoma_10y_spl2  %>%
+                     group_by(fu) %>%
+                     mutate(interval =group_indices()) ## equivalent to Stata's egen interval=group()
+
+##(a)-ii.Fit a model with a parameter for each interval
+## glm -1 is suppress constant term, equivalent to Stata's nocons
+poisson_a <- glm( d ~ as.factor(interval) -1 + offset( log(pt)), 
                   family = poisson,
                   data = melanoma_10y_spl2 )
 summary(poisson_a)
 eform(poisson_a)
+
+## TODO: already predict, but how to put it back to original data?
+##  predict the baseline (one parameter for each interval)
+newdata <- expand.grid(interval = as.factor(1:120), pt = 1 ) ## to have log(pt) = 0
+
+haz_grp <- predict(poisson_a, newdata = newdata, type = "response")
+
+melanoma_10y_spl2 <- melanoma_10y_spl2 %>% 
+                     mutate( haz_grp_1k= haz_grp * 1000,
+                             midtime = (start + t)/2)
+
+ggplot(data = melanoma_10y_spl2, aes(x=start, y = haz_grp_1k))
