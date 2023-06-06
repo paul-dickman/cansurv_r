da6ce20ad1db982b2fceee0259de6eb0a29500df
Enoch Yi-Tung Chen <enochytchen@gmail.com>
2023-06-06 12:14:58 +0200

    Make the popmort file into ratetable for the relsurv pkg

---
+++ solutions/make_ratetable.R
@@ -0,0 +1,31 @@
+## Make ratetable
+## Created: 2023-06-05 Joshua Entrop 
+## Edited:  2023-06-06 Enoch Chen: minor changes
+###############################################################################
+## Load the packages
+library(haven)
+library(tidyverse)
+
+## Read the popmort file
+popmort <- read_dta("popmort.dta")
+
+## Prepare popmort file for making ratetable
+popmort_wide <- pivot_wider(popmort,
+                            id_cols = c("sex", "_age"),
+                            names_from  = "_year",
+                            values_from = "prob")
+
+popmort_males <- popmort_wide %>% 
+                 filter(sex == 1)           %>% 
+                 select(-c("sex", "_age"))  %>% 
+                 as.matrix()
+
+popmort_females <- popmort_wide %>% 
+                   filter(sex == 2)          %>% 
+                   select(-c("sex", "_age")) %>% 
+                   as.matrix()
+
+ratetable <- transrate(popmort_males,
+                       popmort_females,
+                       yearlim = c(1951, 2000),
+                       int.length = 1)
